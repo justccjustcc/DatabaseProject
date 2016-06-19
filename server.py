95276e24d45e341d3f60f94915809ee048737127
justccjustcc <cc3701@columbia.edu>
2016-06-19 18:09:06 -0400

    update

--- server.py
+++ server.py
@@ -341,6 +341,7 @@ def recommend():
     if input == "age":
         user = g.conn.execute("SELECT age FROM users WHERE uid = %s", input)
         user_age = user.age
+        print user_age
         user.close()
         users = g.conn.execute('''SELECT M.mid, M.mname, M.year, M.rating, AVG(R.score) AS ave
         FROM users U, rate R, movie M
